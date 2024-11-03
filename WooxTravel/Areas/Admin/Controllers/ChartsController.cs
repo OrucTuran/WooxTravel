72a237da45d836356476ae3df885cbe4c4e7e6fe
oructuran0 <orctrn193@gmail.com>
2024-11-03 21:21:49 +0300

    Admin - Grafikler bolumu guncellendı
    Çizgi-bar-coklu cizgi-coklu bucuk- donut grafikleri eklendi

---
+++ WooxTravel/Areas/Admin/Controllers/ChartsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WooxTravel.Context;
+using WooxTravel.Entities;
+
+namespace WooxTravel.Areas.Admin.Controllers
+{
+    public class ChartsController : Controller
+    {
+        // GET: Admin/ChartsController
+        TravelContext context = new TravelContext();
+        public ActionResult LineChart()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public ActionResult BarChart()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public ActionResult MultiLineChart()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public ActionResult MultiBarChart()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public ActionResult DonutChart()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+    }
+}
